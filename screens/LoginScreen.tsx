848c3876d6ca3c34c1fd7eae40b3933f9df3007b
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-07-23 21:55:40 -0300

    inicio do projeto

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, StyleSheet, Alert } from 'react-native';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const fazerLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, senha);
+      navigation.replace('ChatList'); // vamos criar essa tela depois
+    } catch (erro: any) {
+      Alert.alert('Erro ao fazer login', erro.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>MeuZap</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Entrar" onPress={fazerLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  titulo: { fontSize: 30, textAlign: 'center', marginBottom: 20 },
+  input: { marginBottom: 15, borderWidth: 1, padding: 10, borderRadius: 5 },
+});
